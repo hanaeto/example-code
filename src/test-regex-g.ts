bec022b4451950dfd414e00bc15968f07250fefe
Gihwan <hanaeto@gmail.com>
2020-05-06 11:57:47 +0900

    Initial commit

---
+++ src/test-regex-g.ts
@@ -0,0 +1,16 @@
+import { inspect } from "util";
+
+const regex: RegExp = /such/ig;
+
+const testValue1: string = 'You are such a great!!';
+const testValue2: string = 'It was such a nice day';
+
+// DESC 변수화한 객체로 test 실행
+console.log( regex.test( testValue1 ) );
+console.log( regex.test( testValue2 ) );
+
+console.log( '' );
+
+// DESC 해결 방법
+console.log( ( new RegExp( regex ) ).test( testValue1 ) );
+console.log( ( new RegExp( regex ) ).test( testValue2 ) );
